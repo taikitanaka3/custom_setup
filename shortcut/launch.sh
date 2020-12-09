4168eab2bfa171f9ec539707ea20a7644c315356
taikitanaka <ttatcoder@outlook.jp>
2020-12-09 15:26:49 +0900

    add aloas

--- shortcut/launch.sh
+++ shortcut/launch.sh
@@ -1,3 +1,3 @@
 source ~/workspace/autoware.proj.jpntaxi/install/setup.bash
-MAP_DIR=/home/tanaka-desk/workspace/AutonomousDrivingScenarios/map/nishishinjuku_kddi
+MAP_DIR=/home/$USER/workspace/AutonomousDrivingScenarios/map/nishishinjuku_aisan
 roslaunch autoware_launch planning_simulator.launch map_path:=$MAP_DIR vehicle_model:=jpntaxi sensor_model:=aip_xx1
--- shortcut/task.sh
+++ shortcut/task.sh
@@ -1,3 +1,4 @@
 
 cd ~/workspace/autoware.proj.jpntaxi
-colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release --catkin-skip-building-tests --symlink-install --packages-select=scenario_common
+PACKAGES=foa_quick
+colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release --catkin-skip-building-tests --symlink-install --packages-up-to $PACKAGES
